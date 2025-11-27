2cb20e204281f44f3214be35205732e0083637f9
Your Name <abhishek708158@gmail.com>
2025-11-27 18:05:02 +0530

    save

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Login/Login";
 
 const App = () => {
-  const url = "https://tomato-gxe4.onrender.com/";
+  const url = "https://tomato-gxe4.onrender.com";
   return (
     <div>
       <ToastContainer />
